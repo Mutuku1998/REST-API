80c841557d21936a74ea5af360e82c130bd74dca
mutuku1998 <martinmutuku1998@gmail.com>
2024-01-09 07:39:11 +0300

    m

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 
 const express = require('express');
 const connection=require('./connection.js')
+const cors = require('cors')
 const app =express();
+
 const productRoute = require ('./routes/product.js')
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
 
 //routes
-
+app.use(cors());
 app.use('/products',productRoute);
 module.exports = app;
