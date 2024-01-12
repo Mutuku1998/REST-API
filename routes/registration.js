20890361cb04f5b3576f6759d80b1ff0b4be13d7
mutuku1998 <martinmutuku1998@gmail.com>
2024-01-12 13:14:58 +0300

    m

--- routes/registration.js
+++ routes/registration.js
@@ -4,21 +4,34 @@ const bcrypt = require('bcryptjs');
 const salt = bcrypt.genSaltSync(10);
 const router = express.Router();
 
-router.post('/user',  (req, res, next) => {
-
+router.post('/user', (req, res, next) => {
     try {
         const password = req.body.password;
         const encryptedPassword = bcrypt.hashSync(password, salt);
 
-        const sql = 'INSERT INTO registration (name, email, password) VALUES (?, ?, ?)';
-        const values = [req.body.name, req.body.email, encryptedPassword];
+        const email = req.body.email; // Extract email from req.body
 
-        connection.query(sql, values, (err, data) => {
+        // Check if user already exists
+        const checkUser = 'SELECT * FROM registration WHERE email=?';
+        connection.query(checkUser, [email], function (err, rows) {
             if (err) {
-                console.error('Database Error:', err);
-                return res.status(500).json({ error: 'Error inserting data into the database' });
+                return res.status(401).json({ message: 'Error in getting email' });
+            } else {
+                if (rows.length) {
+                    return res.status(400).json({ message: 'Email already exists' });
+                } else {
+                    const sql = 'INSERT INTO registration (name, email, password) VALUES (?, ?, ?)';
+                    const values = [req.body.name, email, encryptedPassword];
+
+                    connection.query(sql, values, (err, data) => {
+                        if (err) {
+                            console.error('Database Error:', err);
+                            return res.status(500).json({ error: 'Error inserting data into the database' });
+                        }
+                        return res.status(200).json({ success: true, data: data });
+                    });
+                }
             }
-            return res.status(200).json({ success: true, data: data });
         });
     } catch (error) {
         console.error('Hashing Error:', error);
